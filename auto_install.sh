6258dcd301f48bfd1294803416d181b6ae1f2f67
erinlxy <107250566+erinlxy@users.noreply.github.com>
2022-07-18 16:07:10 +0800

    Create auto_install.sh
    启动文件

---
+++ auto_install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "#### install docker & docker-compose ####"
+sudo sh setupdocker.sh  &&  rm -rf setupdocker.sh
+
+echo "#### create a soft link from docker-compose to dc #### "
+cp ./dc.sh /docker/dc.sh
+chmod +x /docker/dc.sh
+ln -sf /docker/dc.sh  /usr/bin/dc
+
+echo "#### pull images from docker ####"
+echo "####      make contains      ####"
+cp ./docker-compose.yml /docker/smartsmoke/docker-compose.yml
+dc up -d
